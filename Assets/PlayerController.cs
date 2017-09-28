e10c163ce975ac4c1f145e66ec9f3b11ca76d07d
Daniel Richtersz <d.richtersz@link-it.nl>
2017-09-28 11:47:13 +0200

    Init Part 1 and 2 of the tutorial 'Unity 5 - Roll a Ball game' on youtube.com

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody playerRigidBody;
+
+    private void Start()
+    {
+        playerRigidBody = GetComponent<Rigidbody>();
+    }
+
+    //Physics code
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+        playerRigidBody.AddForce(movement * speed);
+    }
+}
